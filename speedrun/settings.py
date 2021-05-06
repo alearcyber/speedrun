63a355364cfea5cc397d5828a74a39de44dc3d4f
alearcyber <alear@cyberprotex.com>
2021-05-05 22:45:05 -0500

    testing custom page

--- speedrun/settings.py
+++ speedrun/settings.py
@@ -31,6 +31,7 @@ DEBUG = os.getenv("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "True"
+#DEVELOPMENT_MODE = True  # comment out before pushing
 
 # Application definition
 
